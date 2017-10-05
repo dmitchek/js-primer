781e15ee78a1cd2bee56611a52afa8c771025de3
David Mitchell <dmitchek@gmail.com>
2017-10-05 13:50:35 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h2>These are some of my favorite colors:</h2>
+    <!-- Step 1. How can we make this more efficient? -->
+    <ul>
+      <li>Red</li>
+      <li>Green</li>
+      <li>Blue</li>
+      <li>Yellow</li>
+      <li>Orange</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
